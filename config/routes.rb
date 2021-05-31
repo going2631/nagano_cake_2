36c0bfd8460515baf56c944db82c464e3c01e8a2
going2631 <tomo.moedora1212@gnmail.com>
2021-05-31 05:14:15 +0000

    [Add]管理画面/商品新規登録画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  
+  namespace :admin do
+    get "/" => "homes#top"
+    resources :items
+    resources :genres, only: [:index, :create, :edit, :update]
+    
+  end
 end
